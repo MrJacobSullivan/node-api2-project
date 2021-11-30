7383e083cf9106a2e9adb9d70e5de79121aa639c
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-30 16:27:25 -0600

    added [GET] /api/posts/:id endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,12 +3,29 @@ const Posts = require('./posts-model');
 
 const router = express.Router();
 
+// [GET] /api/posts
 router.get('/', async (req, res) => {
   try {
     const posts = await Posts.find();
     res.json(posts);
   } catch (err) {
     res.status(500).json({ message: 'The posts information could not be retrieved' });
+  }
+});
+
+// [GET] /api/posts/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Posts.findById(id);
+
+    if (!post) {
+      res.status(404).json({ message: 'The post with the specified ID does not exist' });
+    } else {
+      res.json(post);
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'The post information could not be retrieved' });
   }
 });
 
